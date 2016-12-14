38bf7cf651bc498f590b979fe9f40cb29b9c5f09
Coultonf <cjrfraser@gmail.com>
2016-12-14 00:37:20 -0700

    Did some basic UI changes, and created temporary hard code list elements with an add button

--- RecipeTesting/ViewController.swift
+++ RecipeTesting/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let array = ["item 1", "item2", "item3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.rowHeight = 70
+        tableView.backgroundView = UIImageView(image:UIImage(named: "breads"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell")! as UITableViewCell
+        cell.textLabel?.text = array[indexPath.item]
+        if(indexPath.item % 2 == 0){
+            cell.backgroundColor = UIColor.clear
+        }
+        else{
+            cell.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        }
+        return cell
+    }
 }
 
